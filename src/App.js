5d402386f081da30451917f03f7ed25e4283c641
anomic84@gmail.com <spencercarlson@Spencers-MacBook-Pro-2.local>
2023-03-16 21:28:48 -0700

    fixed issue with overstretched navbar

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import "./App.css"
 
 const App = () => {
     return (
-        <div className="App">
-        <div className="gradient__bg">
+        <div className="App w-full">
+        <div className="gradient__bg w-full">
             <Navbar />
             <Hero />
         </div>
--- src/containers/navbar/navbar.jsx
+++ src/containers/navbar/navbar.jsx
@@ -8,27 +8,27 @@ import './navbar.css'
 const Navbar = () => {
   const [toggleMenu, setToggleMenu] = useState(false);
   return (
-    <div className="flex justify-between items-start pt-[2rem]">
-      <div className="flex items-center justify-between flex-1">
-        <div className="ml-[2rem] mr-[2rem]">
+    <div className="flex w-full justify-between items-start pt-[2rem]">
+      <div className="flex items-center justify-start">
+        <div className="">
           <p className="text-scred font-manrope font-semibold text-[27px]">Spencer C. Carlson</p>
         </div>
-        <div className="invisible lg:visible flex flex-row items-around
+        <div className="hidden lg:flex flex-row items-around
          text-scred text-lg">
           <p><a className="lg:mx-[1rem] hover:text-scredhover" href="#home">Home</a></p>
           <p><a className="lg:mx-[1rem] hover:text-scredhover" href="#projects">Projects</a></p>
           <p><a className="lg:mx-[1rem] hover:text-scredhover" href="#tech">Technologies</a></p>
           <p><a className="lg:mx-[1rem] hover:text-scredhover" href="#footer">Contact</a></p>
         </div>
-        <div className="invisible text-scred mr-[2rem] lg:visible flex flex-column text-lg  items-center">
+        <div className="hidden text-scred mr-[2rem] lg:flex flex-column text-lg  items-center">
           <p className="flex-nowrap items-center pl-5 text-xl">Made with React   </p>
         </div>
-        <div className="invisible lg:visible flex flex-row  justify-between">
-        <FaReact className=" text-scred"/>
-          <SiTailwindcss className=" text-scred"/>
-          </div>
+        <div className='hidden lg:flex'>
+          <FaReact className=" text-scred" />
+          <SiTailwindcss className=" text-scred" />
+        </div>
       </div>
-      <div className="mr-[2rem] lg:invisible">
+      <div className="mr-[2rem] lg:hidden">
         {toggleMenu
           ? <FaTimes color="#530E1A" size={27} onClick={() => setToggleMenu(false)} />
           : <FaBars color="#530E1A" size={27} onClick={() => setToggleMenu(true)} />}
