2ffae2d4e5dd31853504f40a20bac37531260e33
anomic84@gmail.com <spencercarlson@Spencers-MacBook-Pro-2.local>
2023-03-31 15:12:56 -0700

    fixed all the responsive issues that came with adjusting the navbar

--- src/containers/Navbarr/Navbarr.jsx
+++ src/containers/Navbarr/Navbarr.jsx
@@ -10,18 +10,18 @@ const Navbarr = () => {
 
   return (
     <div className="flex w-full justify-between items-center pt-[1rem] lg:pt-[2rem]">
-      <div className="hidden lg:flex w-full items-center justify-between">
+      <div className="hidden md:flex w-full items-center justify-between">
         <div className="">
           <p className=" text-scred  font-manrope font-semibold text-[27px] pl-4">Spencer C. Carlson</p>
         </div>
-        <div className="hidden lg:flex flex-row items-between
+        <div className="hidden md:flex flex-row items-between
          text-scred  text-lg 2xl:text-2xl">
-          <p><a className="lg:mx-[1rem] hover:text-scredhover" href="#home">Home</a></p>
-          <p><a className="lg:mx-[1rem] hover:text-scredhover" href="#projects">Projects</a></p>
-          <p><a className="lg:mx-[1rem] hover:text-scredhover" href="#tech">Technologies</a></p>
-          <p><a className="lg:mx-[1rem] hover:text-scredhover" href="#contact">Contact</a></p>
+          <p><a className="md:mx-[0.5rem] lg:mx-[1rem] hover:text-scredhover" href="#home">Home</a></p>
+          <p><a className="md:mx-[0.5rem] lg:mx-[1rem] hover:text-scredhover" href="#projects">Projects</a></p>
+          <p><a className="md:mx-[0.5rem] lg:mx-[1rem] hover:text-scredhover" href="#tech">Technologies</a></p>
+          <p><a className="md:mx-[0.5rem] lg:mx-[1rem] hover:text-scredhover" href="#contact">Contact</a></p>
         </div>
-        <div className="hidden xs:flex flex-wrap pr-[16px] md:pr-0 md:flex-nowrap justify-center md:justify-none items-end">
+        <div className="hidden sm:flex flex-wrap pr-[16px] md:pr-0 md:flex-nowrap justify-center md:justify-none items-end">
           <a href="https://github.com/anomic84" className="duration-300 text-scred rounded-[50px] p-[8px] hover:text-whitebg hover:bg-scredhover  cursor-pointer scale-[1.2px]" target="_blank" rel="noopener noreferrer">
             <AiFillGithub className="h-[2rem] w-[2rem]" />
           </a>
@@ -36,14 +36,14 @@ const Navbarr = () => {
         </div>
       </div>
       {/* // TABLET */}
-      
+
       {/* mobile */}
-      <div className="lg:hidden flex justify-between items-center px-4 w-full">
+      <div className="md:hidden flex justify-between items-center px-4 w-full">
         {toggleMenu
           ? ''
-          : <h1 className=" text-scred  font-manrope font-semibold text-[27px] pl-4">Spencer C. Carlson</h1>
+          : <h1 className=" text-scred  font-manrope whitespace-nowrap font-semibold text-[27px] pl-4">Spencer C. Carlson</h1>
         }
-        <div className='flex justify-center w-full'>
+        <div className='flex justify-end w-full'>
           {toggleMenu
             ? <FaTimes color="#530E1A" size={27} onClick={() => setToggleMenu((prev) => !prev)} />
             : <FaBars color="#530E1A" size={27} onClick={() => setToggleMenu((prev) => !prev)} />}
